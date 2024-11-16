cbbcc6c739181af9c36450a939161d7d96fcd091
Sebastian Wojciechowski <01191885@pw.edu.pl>
2024-11-16 10:58:52 +0100

    add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
@@ -14,7 +11,7 @@
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            swiProlog
+            swi-prolog
           ];
         };
       });
