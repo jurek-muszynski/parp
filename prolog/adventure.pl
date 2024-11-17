0e13ec906f60232838852d8f92df4102a2629eb8
Sebastian Wojciechowski <01191885@pw.edu.pl>
2024-11-17 21:00:39 +0100

    update code-locked doors inteface

--- prolog/adventure.pl
+++ prolog/adventure.pl
@@ -42,38 +42,20 @@ finish :-
     write('The game is over. Please enter the "halt." command.'),
     nl.
 
-check_code(X) :-
-    i_am_at(X),
-    encoded_door(X, _, _),
-    write('The door is locked. Enter the code: '),
-    read(InputCode),
-    code(RequiredCode),
-    (   InputCode = RequiredCode
-    ->  write('The code is correct. The door unlocks!'), nl,
-        assert(entered_code(X))
-    ;   write('Incorrect code. The door remains locked.'), nl
-    ).
-
-check_code(_) :-
-    write('There is nowhere you could enter the code here.'), nl.
-
 instructions :-
     nl,
     write('Enter commands using standard Prolog syntax.'), nl,
     write('Available commands are:'), nl,
-    write('start.                        -- to start or reset the game.'), nl,
-    write('n.  s.  e.  w.                -- to go in that direction.'), nl,
-    write('a.  b.  c.  d.  ...           -- to select an interaction'), nl,
-    % write('take(Object).                 -- to pick up an object.'), nl,
-    write('drop(Object).                 -- to put down an object.'), nl,
-    write('inspect(Object).              -- to inspect an object.'), nl,
-    write('look.                         -- to look around you again.'), nl,
-    % write('talk_to(Person).              -- to talk to a person.'), nl,
-    % write('ask(Person, Question).        -- to ask a person a question.'), nl,
-    write('inventory.                    -- to see what you are holding.'), nl,
-    write('instructions.                 -- to see this message again.'), nl,
-    write('halt.                         -- to end the game and quit.'), nl,
-    write('check_code(CurrentLocation).  -- to check the code for a door.'), nl,
+    write('start.                  -- to start or reset the game.'), nl,
+    write('n.  s.  e.  w.          -- to go in that direction.'), nl,
+    write('a.  b.  c.  d.  ...     -- to select an interaction'), nl,
+    write('drop(Object).           -- to put down an object.'), nl,
+    write('inspect(Object).        -- to inspect an object.'), nl,
+    write('look.                   -- to look around you again.'), nl,
+    write('inventory.              -- to see what you are holding.'), nl,
+    write('instructions.           -- to see this message again.'), nl,
+    write('halt.                   -- to end the game and quit.'), nl,
+    write('enter_code(Direction).  -- to enter the code for a door (surround it with parenthesis).'), nl,
     nl.
 
 start :-
